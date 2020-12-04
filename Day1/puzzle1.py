d89ab07e719eb789661f0933f2b191a3575cc89a
farshidjf <75459761+farshidjf@users.noreply.github.com>
2020-12-03 22:37:22 -0500

    Create puzzle1.py

---
+++ Day1/puzzle1.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+def puzzle1(list, total):
+  for i, n1 in enumerate(list):
+    for n2 in list[i+1:]:
+      if n1+n2 == total:
+        return n1*n2
+  return 0
+  
+def read_from_file(file):
+  return [int(x) for small_file.read().splitlines()]
+
+if __name__ == "__main__":
+  total = 2020
+  
+  with open(./small_input) as small_file:
+    list = read_from_file(small_file)
+    print("small input: ", puzzle1(list, total))
+
+  with open(./large_input) as large_file:
+    list = read_from_file(large_file)
+    print("small input: ", puzzle1(list, total))
+    
+  
