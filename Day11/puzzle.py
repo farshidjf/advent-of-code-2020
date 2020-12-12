69d36c9f1e499ea409514bdbde126412cb14c79c
farshidjf <75459761+farshidjf@users.noreply.github.com>
2020-12-11 23:13:59 -0500

    Day11

--- Day11/puzzle.py
+++ Day11/puzzle.py
@@ -1,4 +1,4 @@
-from itertools import takewhile
+from itertools import takewhile, count, product
 
 class WaitingRoom:
     def __init__(self, str, puzzle=1):
@@ -6,31 +6,29 @@ class WaitingRoom:
         self.state = [row for row in str.splitlines()]
         self.nr = len(self.state)
         self.nc = len(self.state[0])
-        neighbor_fun = self.neighbors if puzzle == 1 else self.neighbors2
-        self.neighbor_list = [[list(neighbor_fun(i, j)) for j in range(self.nc)] for i in range(self.nr)]
+        neighbors = self.neighbors1 if puzzle == 1 else self.neighbors2
+        self.neighbors = [[list(neighbors(i, j)) for j in range(self.nc)] for i in range(self.nr)]
         
     def is_occupied(self, i, j): return self.state[i][j] == '#'
     def is_empty(self, i, j): return self.state[i][j] == 'L'
     def is_seat(self, i, j): return self.state[i][j] != '.'
     def is_in_range(self, i, j): return 0 <= i < self.nr and 0 <= j < self.nc
     
-    def neighbors(self, i, j): # puzzle 1
-        return [(row, column)   
-                    for row in range(max(i-1, 0), min(i+1+1, self.nr))
-                        for column in range(max(j-1, 0), min(j+1+1, self.nc))
-                            if (row, column) != (i, j) and self.is_seat(row, column)]
+    deltas = set(product((-1,0,1), repeat=2))-{(0,0)}    # [(0,1), (1,0), (1,1), (0,-1), (-1,0), (-1,-1), (1,-1), (-1,1)]
+
+    def neighbors1(self, i, j): # puzzle 1
+        return [(i+di, j+dj) for di, dj in self.deltas if self.is_in_range(i+di, j+dj)]
     
-    def neighbors2(self, i, j): # puzzle 2, feeling functional today
-        delta = [(0,1), (1,0), (1,1), (0,-1), (-1,0), (-1,-1), (1,-1), (-1,1)]
+    def neighbors2(self, i, j): # puzzle 2
         return filter(lambda x: x is not None, 
                       [next(((ii, jj) for ii, jj in 
                                         takewhile(lambda x: self.is_in_range(*x),
-                                                  ((i+n*di, j+n*dj) for n in range(1, max(self.nr,self.nc))) )
+                                                  ((i+n*di, j+n*dj) for n in count(1)) )
                                       if self.is_seat(ii, jj)), None) 
-                       for di, dj in delta])
+                       for di, dj in self.deltas])
         
     def occupied_neighbors(self, i, j):
-        return [(i, j) for i, j in self.neighbor_list[i][j] if self.is_occupied(i, j)]
+        return [(i, j) for i, j in self.neighbors[i][j] if self.is_occupied(i, j)]
     
     def occupy(self, state, i, j): 
         row = list(state[i])
@@ -68,7 +66,7 @@ folder_name = './Day11/'
 for file_name in file_names:
     with open(folder_name+file_name) as input_file:
         data = input_file.read()
-        room1 = WaitingRoom(data, puzzle=1)
-        print(F'Puzzle 1 - {file_name}: ', room1.run())
-        room2 = WaitingRoom(data, puzzle=2)
-        print(F'Puzzle 2 - {file_name}: ', room2.run())
+        puzzle1 = WaitingRoom(data, puzzle=1)
+        print(F'Puzzle 1 - {file_name}: ', puzzle1.run())
+        puzzle2 = WaitingRoom(data, puzzle=2)
+        print(F'Puzzle 2 - {file_name}: ', puzzle2.run())
